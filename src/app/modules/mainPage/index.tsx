1bf46796f58fa16306384f570644eb24aae3e8c0
Pedro Antonio <pedro.morales@go4digital.com.br>
2024-09-05 17:42:30 -0300

    Aoba

--- src/app/modules/mainPage/index.tsx
+++ src/app/modules/mainPage/index.tsx
@@ -4,7 +4,7 @@ export default function Introduction() {
         <main className="flex h-[85dvh] flex-col items-center justify-center bg-white py-44 px-32 bg-[linear-gradient(to_bottom,rgba(128,128,128,0.04)_1px,transparent_1px),linear-gradient(to_right,rgba(128,128,128,0.04)_1px,transparent_1px)]  bg-[size:32px_32px] bg-[mask-image:linear-gradient(to_bottom,rgba(0,0,0,0)_0%,rgba(0,0,0,1)_100%)]">
             <section className="w-full flex flex-col gap-3 items-center justify-center">
                 <div className="w-fit px-3 shadow border rounded-lg bg-white text-gray-500">Introduction</div>
-                <div className="text-7xl max-w-[60%] text-center font-bold tracking-tight leading-[1.2] bg-gradient-to-r from-gray-500 to-gray-900 bg-clip-text text-transparent animate-float">
+                <div className="text-7xl max-w-[60%] text-center font-bold tracking-tight leading-[1.2] my-1 bg-gradient-to-r from-gray-500 to-gray-900 bg-clip-text text-transparent animate-float">
                     The newest and friendly facilitator for your project design
                 </div>
                 <p className="text-lg font-medium text-gray-400">
